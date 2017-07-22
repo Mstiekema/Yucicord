79ff1fe9b652dfce207e976aab6ac6a8375b0eae
Mstiekema <merstiekema@gmail.com>
2017-07-22 15:13:13 +0200

    Changed embed to serverinfo
    Also removed test commands as they are no longer needed

--- app.js
+++ app.js
@@ -15,24 +15,15 @@ client.on('ready', function() {
 });
 
 client.on('message', function(message) {
-  if (message.content == '!ping') {
-    message.reply('pong');
-  }
-  
-  if (message.content == '!embed') {
+  if (message.content == '!serverinfo') {
+    console.log(message)
     message.channel.send({embed: {
       color: Math.floor(Math.random() * 10000000),
-      title: "Yucicord info",
-      description: "Welcome to the server!",
-      fields: [{
-          name: "User stats",
-          value: 'Users on server: ' + message.guild.members.map(function() {}).length + ' | Users online: '+ message.guild.presences.filter(function(value){return value.status == 'online'}).map(function() {}).length
-        },
-        {
-          name: "Another field",
-          value: "With some test text"
-        }
-      ],
+      "author": {
+        "name": message.guild.name,
+        "icon_url": message.guild.icon
+      },
+      description: 'Users on server: ' + message.guild.members.map(function() {}).length + ' | Users online: '+ message.guild.presences.filter(function(value){return value.status == 'online'}).map(function() {}).length,
       timestamp: new Date(),
       footer: {
         icon_url: client.user.avatarURL,
@@ -40,30 +31,6 @@ client.on('message', function(message) {
       }
     }});
   }
-  
-  if (message.content == '!voice') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => {
-          const ytdl = require('ytdl-core');
-          const streamOptions = { seek: 0, volume: 1 };
-          const stream = ytdl('https://www.youtube.com/watch?v=BeaNRbiiqGg', { filter : 'audioonly' });
-          
-          console.log("Connected to the voice channel")
-          connection.playStream(stream);
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You aren\'t in a voice channel');
-    }
-  }
-  
-  if (message.content == '!leave') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.leave()
-    }
-  }
-  
   songq.sr(message);
 });
 
